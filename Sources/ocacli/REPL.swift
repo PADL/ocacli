70f80734b6f572142b48665083cbdfd9fb3674ff
Luke Howard <lukeh@padl.com>
2025-01-27 17:22:17 +0800

    make init?(hex: String) internal

--- Sources/ocacli/REPL.swift
+++ Sources/ocacli/REPL.swift
@@ -368,3 +368,20 @@ extension CaseIterable {
     return nil
   }
 }
+
+// https://stackoverflow.com/questions/26501276/converting-hex-string-to-nsdata-in-swift
+extension Data {
+  init?(hex: String) {
+    guard hex.count.isMultiple(of: 2) else {
+      return nil
+    }
+
+    let chars = hex.map { $0 }
+    let bytes = stride(from: 0, to: chars.count, by: 2)
+      .map { String(chars[$0]) + String(chars[$0 + 1]) }
+      .compactMap { UInt8($0, radix: 16) }
+
+    guard hex.count / bytes.count == 2 else { return nil }
+    self.init(bytes)
+  }
+}
