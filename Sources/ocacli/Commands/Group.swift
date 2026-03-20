ae81052580e924ca78ea2c46258cf1a7e733a8e4
Luke Howard <lukeh@padl.com>
2026-03-20 12:46:35 +1100

    add get-members/get-group-controller commands for OcaGroup

--- Sources/ocacli/Commands/Group.swift
+++ Sources/ocacli/Commands/Group.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2024 PADL Software Pty Ltd
+// Copyright (c) 2024-2026 PADL Software Pty Ltd
 //
 // Licensed under the Apache License, Version 2.0 (the License);
 // you may not use this file except in compliance with the License.
@@ -16,6 +16,50 @@
 
 import Foundation
 import SwiftOCA
+
+struct GetMembers: REPLCommand, REPLClassSpecificCommand {
+  static let name = ["get-members"]
+  static let summary = "Get group members"
+
+  static var supportedClasses: [OcaClassIdentification] {
+    [OcaGroup.classIdentification]
+  }
+
+  init() {}
+
+  func execute(with context: Context) async throws {
+    let group = context.currentObject as! OcaGroup
+    let members: [OcaRoot] = try await group.resolveMembers()
+    for member in members {
+      let rolePath = try await member
+        .getRolePathString(flags: context.contextFlags.cachedPropertyResolutionFlags)
+      context.print(rolePath)
+    }
+  }
+
+  static func getCompletions(with context: Context, currentBuffer: String) -> [String]? { nil }
+}
+
+struct GetGroupController: REPLCommand, REPLClassSpecificCommand {
+  static let name = ["get-group-controller"]
+  static let summary = "Get group controller"
+
+  static var supportedClasses: [OcaClassIdentification] {
+    [OcaGroup.classIdentification]
+  }
+
+  init() {}
+
+  func execute(with context: Context) async throws {
+    let group = context.currentObject as! OcaGroup
+    let groupController: OcaRoot = try await group.resolveGroupController()
+    let rolePath = try await groupController
+      .getRolePathString(flags: context.contextFlags.cachedPropertyResolutionFlags)
+    context.print(rolePath)
+  }
+
+  static func getCompletions(with context: Context, currentBuffer: String) -> [String]? { nil }
+}
 
 struct AddMember: REPLCommand, REPLCurrentBlockCompletable, REPLClassSpecificCommand {
   static let name = ["add-member"]
@@ -53,7 +97,7 @@ struct DeleteMember: REPLCommand, REPLCurrentBlockCompletable, REPLClassSpecific
 
   func execute(with context: Context) async throws {
     let group = context.currentObject as! OcaGroup
-    try await group.add(member: member.objectNumber)
+    try await group.delete(member: member.objectNumber)
   }
 
   static func getCompletions(with context: Context, currentBuffer: String) -> [String]? { nil }
